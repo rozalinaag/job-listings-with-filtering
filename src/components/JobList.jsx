1020c711da7cd4748ad4f2bd59c0f58aaac84b63
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-10-04 12:00:58 +0300

    first commit

---
+++ src/components/JobList.jsx
@@ -0,0 +1,18 @@
+import {useSelector} from 'react-redux';
+import { selectAllPositions } from 'store/positions/position-selectors';
+import { JobPosition } from './JobPosition';
+
+const JobList = () => {
+
+  const positions = useSelector(selectAllPositions);
+
+  return (
+    <div className='job-list'>
+      {positions.map(item => (
+        <JobPosition key={item.id} {...item} />
+      ))}
+    </div>
+  )
+}
+
+export {JobList};
---
+++ src/store/positions/position-actions.js
@@ -0,0 +1,9 @@
+export const ADD_POSITIONS = 'ADD_POSITIONS';
+
+export const addPositions = (positions) => (
+  {
+    type: ADD_POSITIONS, 
+    positions
+  }
+);
+
