82d79d1314c63828b977721a3831bd602fb00b26
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-10-14 12:27:25 +0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './store';
-
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
